3615f023fe15486eb7da552c89a719805368dc84
bonjour123265 <159901588+bonjour123265@users.noreply.github.com>
2024-02-15 09:57:18 +0100

    chocolatine
    c les meilleur

---
+++ stage-3-me/TestNouvelleAPI.java
@@ -0,0 +1,33 @@
+package org.acme;
+
+import io.quarkus.arc.properties.IfBuildProperty;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/test")
+public class TestNouvelleAPI {
+
+    @Path("{param}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String testeExpressionLitterale(String param) {
+        String res = "contient que des caracteres autorisés ? ";
+        res += neContientQueDesCaracteresAutorises(param);
+        return res;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "salut machin !";
+    }
+
+    public static boolean neContientQueDesCaracteresAutorises(String chaine) {
+        // Utilisation d'une expression régulière pour vérifier si la chaîne contient
+        // uniquement des chiffres
+        return chaine.matches("[0-9a-z()+\\-*/]+");
+    }
+
+}
